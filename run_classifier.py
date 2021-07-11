8ff3fdc6cfbc1b163931e0e04c040ddaedef18fb
s2051012 <s2051012@ed.ac.uk>
2021-07-11 18:31:20 +0100

    ub-2

--- run_classifier.py
+++ run_classifier.py
@@ -105,7 +105,7 @@ for i in range(EPOCH):
             utt_pointer = temp.split(':')[1][:-3].replace('\\r', '')  # pointer to the utterance
             # print(temp, utt_file_loc)
 
-            with open('./data' + utt_file_loc, 'rb') as ark_file:
+            with open('../remote/data' + utt_file_loc, 'rb') as ark_file:
                 # mlp use: '../remote/data' + utt_file_loc
                 # ubuntu use: './data' + utt_file_loc
                 ark_file.seek(int(utt_pointer))
@@ -161,7 +161,7 @@ for i in range(EPOCH):
             utt_pointer = temp.split(':')[1][:-3].replace('\\r', '')  # pointer to the utterance
             # print(didxutt_file_loc, utt_pointer)
             # According to the file name and pointer to get the matrix
-            with open('./data' + utt_file_loc, 'rb') as ark_file:
+            with open('../remote/data' + utt_file_loc, 'rb') as ark_file:
                 # mlp use: '../remote/data' + utt_file_loc
                 # ubuntu use: './data' + utt_file_loc
                 ark_file.seek(int(utt_pointer))
