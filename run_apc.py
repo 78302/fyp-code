13ee966c77e7eba382fc1e02484c2e0702f7c571
s2051012 <s2051012@ed.ac.uk>
2021-07-21 17:47:49 +0100

    ub

--- run_apc.py
+++ run_apc.py
@@ -10,40 +10,65 @@ import argparse
 # Deal with the use input parameters
 # ideal input: epoch, learning-rate, K, input-size, hidden-size, train-scp-path, dev-scp-path, layers
 parser = argparse.ArgumentParser(description='Parse the net paras')
+parser.add_argument('--name', '-n', help='Name of the Model, required', required=True)
 parser.add_argument('--learning_rate', '-lr', help='Learning rate, not required', type=float, default=0.001)
-parser.add_argument('--epoch', '-e', help='Epoch, not required', type=int, default=100)
+parser.add_argument('--epoch', '-e', help='Epoch, not required', type=int, default=1)
 parser.add_argument('--gap', '-k', help='Position in origin where the first prediction correspond to, not required', type=int,  default=2)
 parser.add_argument('--input_size', '-is', help='Input dimension, not required', type=float, default=40)
 parser.add_argument('--hidden_size', '-hs', help='Hidden vector dimension, not required', type=float, default=512)
-parser.add_argument('--layers', '-l', help='Number of LSTM layers used, not required', type=int, default=1)
-parser.add_argument('--train_scp_path', '-tsp', help='Path of the input training scp file, not required', default='./data/raw_fbank_train_si284.scp')
-parser.add_argument('--dev_scp_path', '-dsp', help='Path of the input validation scp file, not required', default='./data/raw_fbank_dev.scp')
+parser.add_argument('--layers', '-l', help='Number of LSTM layers used, not required', type=int, default=4)
+parser.add_argument('--type', '-t', help='Ubuntu type or mlp type, not required default is Ubuntu type', type=int,  default=1)
+parser.add_argument('--order', '-o', help='The order of the epoch, not required', type=int,  default=1)
+
+# parser.add_argument('--train_scp_path', '-tsp', help='Path of the input training scp file, not required', default='./data/raw_fbank_train_si284.scp')
+# parser.add_argument('--dev_scp_path', '-dsp', help='Path of the input validation scp file, not required', default='./data/raw_fbank_dev.scp')
 # required=True
 args = parser.parse_args()
 
 # Assign parameters
+NAME = args.name
 LEARNING_RATE = args.learning_rate
 EPOCH = args.epoch
 K = args.gap
 INPUT_SIZE = args.input_size
 HIDDEN_SIZE = args.hidden_size
 LAYERS = args.layers
-TRAIN_SCP_PATH = args.train_scp_path
-DEV_SCP_PATH = args.dev_scp_path
+TYPE = args.type
+ORDER = args.order
+
+# Decide the file path under different environment
+# Python do not have switch case, use if else instead
+if TYPE == 1:  # under Ubbuntu test environment
+    TRAIN_SCP_PATH = './data/raw_fbank_train_si284.1.scp'
+    DEV_SCP_PATH = './data/raw_fbank_train_si284.1.scp'
+    UTT_RELATIVE_PATH = './data'  # relative path of ark file under Ubuntu environment
+    C = 28  # cutting position to divide the list
+else:
+    TRAIN_SCP_PATH = './data/raw_fbank_train_si284.scp'
+    DEV_SCP_PATH = './data/raw_fbank_dev.scp'
+    UTT_RELATIVE_PATH = '../remote/data'
+    C = 18
+
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")  #
 rnn = toy_lstm(INPUT_SIZE=INPUT_SIZE, HIDDEN_SIZE=HIDDEN_SIZE, LAYERS=LAYERS).to(device)
 optimizer = torch.optim.Adam(rnn.parameters(), lr=LEARNING_RATE)  # optimize all parameters
-loss_func = nn.MSELoss()
 # Learning rate decay schedule
-mult_step_scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer,
-                                                           milestones=[EPOCH // 2, EPOCH // 4 * 3], gamma=0.5)
-# print(LEARNING_RATE, EPOCH, K, INPUT_SIZE, HIDDEN_SIZE, LAYERS, SCP_PATH)
-# print(rnn)
+# mult_step_scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer,
+#                                                            milestones=[EPOCH // 2, EPOCH // 4 * 3], gamma=0.5)
 
-## LEARNING_RATE = 0.001
-## EPOCH = 1  # change to 100 when using sbatch
-## K = 2
+optimizer = torch.optim.Adam(rnn.parameters(), lr=LEARNING_RATE)  # optimize all parameters
+
+from functions import load_model
+if ORDER != 1:  # load the previous model
+    path = './pretrain_model/model/Epoch{:d}_{:s}.pth.tar'.format(ORDER-1, NAME)
+    # print(path)
+    rnn, optimizer = load_model(path, rnn, optimizer) # load the model
+# exit()
+
+optimizer = torch.optim.Adam(rnn.parameters(), lr=LEARNING_RATE)  # optimize all parameters
+loss_func = nn.MSELoss()
+
 
 # Train + Dev
 train_loss = []
@@ -61,23 +86,15 @@ for i in range(EPOCH):
     # Use the total scp files
     # Read data index from the total scp file
     with open(TRAIN_SCP_PATH, 'rb') as scp_file:
-        # mlp file path: ./data/raw_fbank_train_si284.scp
-        # test file path: ./data/raw_fbank_train_si284.1.scp
-        # win file path: ./data/raw_fbank_train_si284.1.scp
         lines = scp_file.readlines()
-        for line in lines[:1]:
+        for line in lines[]:
             temp = str(line).split()[1]
-            file_loc = temp.split(':')[0][28:]
-            # ark file path; keep [18:]
-            # win keep[28:]
+            file_loc = temp.split(':')[0][C:]
             pointer = temp.split(':')[1][:-3].replace('\\r', '')  # pointer to the utterance
             # print(file_loc, pointer)
 
             # According to the file name and pointer to get the matrix
-            with open('./data' + file_loc, 'rb') as ark_file:
-                # mlp file path: '../remote/data' + file_loc
-                # test file path: '../fyp_code/data' + file_loc
-                # win file path: './data' + file_loc
+            with open(UTT_RELATIVE_PATH + file_loc, 'rb') as ark_file:
                 ark_file.seek(int(pointer))
                 utt_mat = kaldiark.parse_feat_matrix(ark_file)
 
@@ -106,18 +123,15 @@ for i in range(EPOCH):
         # test file path: ./data/raw_fbank_train_si284.2.scp
         # win file path: ./data/raw_fbank_train_si284.2.scp
         lines = scp_file.readlines()
-        for line in lines[:1]:
+        for line in lines[]:
             temp = str(line).split()[1]
-            file_loc = temp.split(':')[0][28:]
+            file_loc = temp.split(':')[0][C:]
             # ark file path; keep [18:]
             # win file keep [28:]
             pointer = temp.split(':')[1][:-3].replace('\\r', '')  # pointer to the utterance
 
             # According to the file name and pointer to get the matrix
-            with open('./data' + file_loc, 'rb') as ark_file:
-                # mlp file path: '../remote/data' + file_loc
-                # test file path: '../fyp_code/data' + file_loc
-                # win file path: './data' + file_loc
+            with open(UTT_RELATIVE_PATH + file_loc, 'rb') as ark_file:
                 ark_file.seek(int(pointer))
                 utt_mat = kaldiark.parse_feat_matrix(ark_file)
 
@@ -147,7 +161,7 @@ for i in range(EPOCH):
     if ((i + 1) % 1 == 0):
         torch.save({'epoch': i + 1, 'state_dict': rnn.state_dict(), 'train_loss': train_loss,
                     'valid_loss': valid_loss, 'optimizer': optimizer.state_dict()},
-                    './model/Epoch{:d}.pth.tar'.format((i + 1), min_valid_loss))
+                    './pretrain_model/model/Epoch{:d}_{:s}.pth.tar'.format(ORDER, NAME))
 
     # Log
     log_string = ('iter: [{:d}/{:d}], train_loss: {:0.6f}, valid_loss: {:0.6f}, '
@@ -158,27 +172,27 @@ for i in range(EPOCH):
                                                                   optimizer.param_groups[0]['lr'],
                                                                   (end-tmp))
     tmp = end
-    mult_step_scheduler.step()  # 学习率更新
+    # mult_step_scheduler.step()  # 学习率更新
     print(log_string)  # 打印日志
 
 print(end-start)
 
-# Draw the train loss
-import numpy as np
-import matplotlib
-matplotlib.use('Agg')
-import matplotlib.pyplot as plt
-y = train_loss
-x = np.arange(0,len(train_loss))
-fig, ax = plt.subplots(figsize=(14,7))
-ax.plot(x,y,'r--',label='type1')
-
-ax.set_title('Loss',fontsize=18)
-ax.set_xlabel('epoch', fontsize=18,fontfamily = 'sans-serif',fontstyle='italic')
-ax.set_ylabel('loss', fontsize='x-large',fontstyle='oblique')
-ax.legend()
-
-plt.savefig("loss.pdf")
+# # Draw the train loss
+# import numpy as np
+# import matplotlib
+# matplotlib.use('Agg')
+# import matplotlib.pyplot as plt
+# y = train_loss
+# x = np.arange(0,len(train_loss))
+# fig, ax = plt.subplots(figsize=(14,7))
+# ax.plot(x,y,'r--',label='type1')
+#
+# ax.set_title('Loss',fontsize=18)
+# ax.set_xlabel('epoch', fontsize=18,fontfamily = 'sans-serif',fontstyle='italic')
+# ax.set_ylabel('loss', fontsize='x-large',fontstyle='oblique')
+# ax.legend()
+#
+# plt.savefig("loss.pdf")
 
 # from functions import load_model
 # PATH = './model/Epoch1.pth.tar'
