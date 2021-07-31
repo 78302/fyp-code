72db556c6b723836fa03238ff98b4f71ae7d1c06
s2051012 <s2051012@ed.ac.uk>
2021-07-31 12:27:20 +0100

    ub

--- job_raw_classifier_lstm.sh
+++ job_raw_classifier_lstm.sh
@@ -24,7 +24,8 @@ sshfs -o IdentityFile=/home/s2051012/msc/id_rsa -p 522 msc@129.215.91.172:/ /hom
 # ls msc/fyp-code/
 cd msc/fyp-code/
 # python3 run_apc.py
-python3 run_classifier.py -n 'LC_lstm_lr0001' -lr 0.001 -is 512 -p './pretrain_model/model/Epoch0.pth.tar' -t 0 -o $1 > job_classifier_lstm_lr0001_epoch$1.out
+# python3 run_classifier.py -n 'LC_lstm_lr0001' -lr 0.001 -is 512 -p './pretrain_model/model/Epoch0.pth.tar' -t 0 -o $1 > job_classifier_lstm_lr0001_epoch$1.out
+python3 train_classifier.py -n 'LC_lstm' -e 20 -lr 0.001 -is 512 -t 0 > job_classifier_lstm.out
 # echo 'Model pretrained!'
 
 # Unmount it
