b9851f356ed174b932f77c8f0ff9e47da2703bd2
s2051012 <s2051012@ed.ac.uk>
2021-08-03 10:03:20 +0100

    ub

---
+++ run_kmeans_apc.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Author: Z
+# Version: 1.0
+# Created Time: 2021/07/03
+# Config the running environment of the APC model and train the APC in slurm
+
+cd /
+cd /home/s2051012
+. msc/toolchain.rc
+. msc/venv/bin/activate
+sshfs -o IdentityFile=/home/s2051012/msc/id_rsa -p 522 msc@129.215.91.172:/ /home/s2051012/msc/remote
+cd msc/fyp-code/
+python3 testKM.py -n 'km_apc_'$1'Centers' -e 10 -t 0 -p './pretrain_model/model/Epoch100_WSJ_APC_lr0001.pth.tar' -k $1 > job_km_rawLSTM_k$1.out
+cd /
+cd /home/s2051012
+fusermount -u msc/remote
