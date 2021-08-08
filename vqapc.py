3d567d51b8f872a6d0bf9879b85db2707925f7a8
s2051012 <s2051012@ed.ac.uk>
2021-08-08 23:46:20 +0100

    ub

--- vqapc.py
+++ vqapc.py
@@ -239,10 +239,10 @@ class toy_vqapc(nn.Module):
         self.h_s = None
         self.h_c = None
 
-    def forward(self, x):
+    def forward(self, x, testing):
         r_out, (h_s, h_c) = self.rnn(x)
 
-        logits_BxLxC, rnn_outputs_BxLxH = self.vq_layer(r_out, True)
+        logits_BxLxC, rnn_outputs_BxLxH = self.vq_layer(r_out, testing)
         output = self.fc(rnn_outputs_BxLxH)
         return output
 
