4b93b5e9dab5e592a2aaa7ac90cf4b3da437b0be
s2051012 <s2051012@ed.ac.uk>
2021-08-08 23:30:48 +0100

    ub

--- testKM.py
+++ testKM.py
@@ -95,7 +95,7 @@ for e in range(EPOCH):
             if PRETRAIN_PATH:
                 utt_mat = torch.Tensor(utt_mat).to(DEVICE)
                 utt_mat = torch.unsqueeze(utt_mat, 0)
-                rep = apc(utt_mat)
+                rep = apc(x=utt_mat, testing=True)
                 rep = rep.cpu()
                 utt_mat = rep.detach().numpy()[0]
 
